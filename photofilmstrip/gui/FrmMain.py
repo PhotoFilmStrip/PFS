e9419c4b63f911070f621026fad9adbd6d0b0578
jensgoe <mail@jensgoepfert.de>
2017-03-03 08:17:18 +0100

    Release updates
    * version number to 3.0.2 increased
    * current year in about dialog

--- photofilmstrip/gui/FrmMain.py
+++ photofilmstrip/gui/FrmMain.py
@@ -288,7 +288,7 @@ class FrmMain(wx.Frame, Observer, WxVisualJobManager):
         info = wx.AboutDialogInfo()
         info.Name = Constants.APP_NAME
         info.Version = Constants.APP_VERSION_EX
-        info.Copyright = u"(C) 2013 %s" % Constants.DEVELOPERS[0]
+        info.Copyright = u"(C) 2017 %s" % Constants.DEVELOPERS[0]
         info.Description = wordwrap(_("PhotoFilmStrip creates movies out of your pictures in just 3 steps. First select your photos, customize the motion path and render the video. There are several output possibilities for VCD, SVCD, DVD up to FULL-HD."), 
                                     350, 
                                     wx.ClientDC(self))
