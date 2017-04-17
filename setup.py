a61553f344258a2436cf201c0d09ae8bf60fc0ae
YoandyShyno <yoandy.shyno@gmail.com>
2017-04-17 14:18:13 +0200

    add photofilmstrip-auto to build list.

--- setup.py
+++ setup.py
@@ -362,6 +362,7 @@ platform_data = []
 if os.name == "nt":
     platform_scripts.append("windows/photofilmstrip.bat")
     platform_scripts.append("windows/photofilmstrip-cli.bat")
+    platform_scripts.append("windows/photofilmstrip-auto.bat")
     platform_data.append((os.path.join("share", "doc", "photofilmstrip"), ["windows/photofilmstrip.chm"]))
 else:
     platform_data.append(("share/applications", ["data/photofilmstrip.desktop"]))
@@ -462,6 +463,7 @@ setup(
     scripts=[
              "scripts/photofilmstrip",
              "scripts/photofilmstrip-cli",
+             "scripts/photofilmstrip-auto"
     ] + platform_scripts,
 
     name = Constants.APP_NAME.lower(),
