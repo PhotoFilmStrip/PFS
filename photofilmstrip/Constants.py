2043ed6ff9fda9acc78ef2cc536d917a9632424f
jensgoe <mail@jensgoepfert.de>
2017-11-12 11:41:55 +0100

    version set to 3.1.2

--- photofilmstrip/Constants.py
+++ photofilmstrip/Constants.py
@@ -23,21 +23,20 @@ import os
 import sys
 
 try:
-    from photofilmstrip._scmInfo import SCM_REV # IGNORE:F0401
+    from photofilmstrip._scmInfo import SCM_REV  # IGNORE:F0401
 except ImportError:
     SCM_REV = "src"
 
-
 APP_DIR = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "..")
 
-APP_NAME        = "PhotoFilmStrip"
-APP_VERSION     = "3.1.1"
-APP_VERSION_EX  = "%s-%s" % (APP_VERSION, SCM_REV)
-APP_SLOGAN      = "PhotoFilmStrip - Creates movies out of your pictures."
+APP_NAME = "PhotoFilmStrip"
+APP_VERSION = "3.1.2"
+APP_VERSION_EX = "%s-%s" % (APP_VERSION, SCM_REV)
+APP_SLOGAN = "PhotoFilmStrip - Creates movies out of your pictures."
 APP_DESCRIPTION = """\
 PhotoFilmStrip creates movies out of your pictures in just 3 steps. First select your photos, customize the motion path and render the video. There are several output possibilities for VCD, SVCD, DVD up to FULL-HD.
 """
-APP_URL         = "http://www.photofilmstrip.org"
-DEVELOPERS      = [u"Jens Göpfert", 
-                   u"Markus Wintermann"] 
-TRANSLATORS     = ["Teza Lprod - http://lprod.org", "geogeo - http://www.geogeo.gr"]
+APP_URL = "http://www.photofilmstrip.org"
+DEVELOPERS = [u"Jens Göpfert",
+                   u"Markus Wintermann"]
+TRANSLATORS = ["Teza Lprod - http://lprod.org", "geogeo - http://www.geogeo.gr"]
