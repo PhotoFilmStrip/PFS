6e6eb94bcb5b95264fefdd6723049962f40995f3
jensgoe <mail@jensgoepfert.de>
2017-11-23 23:34:53 +0100

    version bump

--- photofilmstrip/Constants.py
+++ photofilmstrip/Constants.py
@@ -30,13 +30,12 @@ except ImportError:
 APP_DIR = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "..")
 
 APP_NAME = "PhotoFilmStrip"
-APP_VERSION = "3.2.0"
+APP_VERSION = "3.3.0"
 APP_VERSION_EX = "%s-%s" % (APP_VERSION, SCM_REV)
 APP_SLOGAN = "PhotoFilmStrip - Creates movies out of your pictures."
 APP_DESCRIPTION = """\
 PhotoFilmStrip creates movies out of your pictures in just 3 steps. First select your photos, customize the motion path and render the video. There are several output possibilities for VCD, SVCD, DVD up to FULL-HD.
 """
 APP_URL = "http://www.photofilmstrip.org"
-DEVELOPERS = [u"Jens Göpfert",
-                   u"Markus Wintermann"]
+DEVELOPERS = [u"Jens Göpfert"]
 TRANSLATORS = ["Teza Lprod - http://lprod.org", "geogeo - http://www.geogeo.gr"]
