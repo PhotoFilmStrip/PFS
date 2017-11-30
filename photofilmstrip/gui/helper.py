0aa1fe8c08fc1c62b5e8b923973e06b77561a19b
Tobias <mail@tobiasstampfl.de>
2017-11-30 08:08:01 +0100

    redesigned filmstrip (closes #18)
    nicer DnD
    fixed bug with images whose height is smaller than the thumb height

--- photofilmstrip/gui/helper.py
+++ photofilmstrip/gui/helper.py
@@ -19,3 +19,29 @@ def CreateMenuItem(menu, ident, text="", bmp=None, disabledBitmap=None):
         item.SetDisabledBitmap(disabledBitmap)
 
     menu.AppendItem(item)
+
+
+def ChopText(dc, text, maxSize):
+    """
+    Chops the input `text` if its size does not fit in `maxSize`, by cutting the
+    text and adding ellipsis at the end.
+
+    :param `dc`: a `wx.DC` device context;
+    :param `text`: the text to chop;
+    :param `maxSize`: the maximum size in which the text should fit.
+    """
+
+    # first check if the text fits with no problems
+    width, __ = dc.GetTextExtent(text)
+
+    if width <= maxSize:
+        return text, width
+
+    for i in xrange(len(text), -1, -1):
+        s = '%s ... %s' % (text[:i * 33 / 100], text[-i * 67 / 100:])
+
+        width, __ = dc.GetTextExtent(s)
+
+        if width <= maxSize:
+            break
+    return s, width
