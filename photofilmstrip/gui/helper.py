8a3fa37aa4b946da9d07a879e3946ef0adc7478c
jensgoe <mail@jensgoepfert.de>
2024-01-01 23:16:48 +0100

    use native disabled icons within menu bar
    fixes menubar issues with wxPython-4.2.x

--- photofilmstrip/gui/helper.py
+++ photofilmstrip/gui/helper.py
@@ -17,8 +17,8 @@ def CreateMenuItem(menu, ident, text="", bmp=None, disabledBitmap=None):
     if bmp is not None:
         item.SetBitmap(bmp)
 
-    if disabledBitmap is not None:
-        item.SetDisabledBitmap(disabledBitmap)
+#    if disabledBitmap is not None:
+#        item.SetDisabledBitmap(disabledBitmap)
 
     menu.Append(item)
 
